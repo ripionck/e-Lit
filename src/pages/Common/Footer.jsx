340f6c3e36a283f417f0d88abf08447fdaaddd0c
Ripion Chakma <ripionchakma.421@gmail.com>
2024-02-15 22:00:37 +0600

    edited footer

--- src/pages/Common/Footer.jsx
+++ src/pages/Common/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <footer className="bg-gray-100 dark:bg-gray-900 mt-8">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <div className="mx-auto w-full max-w-screen-xl p-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <a href="#" className="flex items-center">
@@ -68,7 +68,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-4" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2024
