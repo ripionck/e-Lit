d0d81c5164076d9ef8d30b7d0e8f20308791973b
Ripion Chakma <ripionchakma.421@gmail.com>
2024-02-29 23:42:27 +0600

    added helmet on not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,20 @@
+import { Helmet } from 'react-helmet';
+
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
-        <p className="text-lg text-gray-700">
-          The page you are looking for does not exist.
-        </p>
+    <>
+      <Helmet>
+        <title>E-Lit Emporium | Not Found</title>
+      </Helmet>
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
+          <p className="text-lg text-gray-700">
+            The page you are looking for does not exist.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default NotFound;
