4efca14bb39141da023b091c34bd4b06edd22141
Ripion Chakma <ripionchakma.421@gmail.com>
2024-02-29 23:47:01 +0600

    added helmet on dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,41 +6,47 @@ import { BiSolidCategory } from 'react-icons/bi';
 import { FaUser, FaUserEdit } from 'react-icons/fa';
 import { TbReportMoney } from 'react-icons/tb';
 import { Outlet } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const Dashboard = () => {
   return (
-    <div className="flex pt-2">
-      <Sidebar aria-label="Default sidebar example">
-        <Sidebar.Items>
-          <Sidebar.ItemGroup>
-            <Sidebar.Item href="/dashboard/authors" icon={HiUser}>
-              All Authors
-            </Sidebar.Item>
-            <Sidebar.Item href="/dashboard/publishers" icon={FaUserEdit}>
-              All Publishers
-            </Sidebar.Item>
-            <Sidebar.Item href="/dashboard/users" icon={FaUser}>
-              All Users
-            </Sidebar.Item>
-            <Sidebar.Item href="/dashboard/books" icon={HiBookOpen}>
-              All Books
-            </Sidebar.Item>
-            <Sidebar.Item href="/dashboard/categories" icon={BiSolidCategory}>
-              All Categories
-            </Sidebar.Item>
-            <Sidebar.Item href="/dashboard/transactions" icon={TbReportMoney}>
-              Transactions
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiInbox} label="3">
-              Inbox
-            </Sidebar.Item>
-          </Sidebar.ItemGroup>
-        </Sidebar.Items>
-      </Sidebar>
-      <div className="flex-1 pl-2">
-        <Outlet />
+    <>
+      <Helmet>
+        <title>E-Lit Emporium | Dashboard</title>
+      </Helmet>
+      <div className="flex pt-2">
+        <Sidebar aria-label="Default sidebar example">
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="/dashboard/authors" icon={HiUser}>
+                All Authors
+              </Sidebar.Item>
+              <Sidebar.Item href="/dashboard/publishers" icon={FaUserEdit}>
+                All Publishers
+              </Sidebar.Item>
+              <Sidebar.Item href="/dashboard/users" icon={FaUser}>
+                All Users
+              </Sidebar.Item>
+              <Sidebar.Item href="/dashboard/books" icon={HiBookOpen}>
+                All Books
+              </Sidebar.Item>
+              <Sidebar.Item href="/dashboard/categories" icon={BiSolidCategory}>
+                All Categories
+              </Sidebar.Item>
+              <Sidebar.Item href="/dashboard/transactions" icon={TbReportMoney}>
+                Transactions
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiInbox} label="3">
+                Inbox
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+        <div className="flex-1 pl-2">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Dashboard;
